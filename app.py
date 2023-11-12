1c91ce98652ca621956876bf0a9698cccd24da21
MANAS-exe <131545414+MANAS-exe@users.noreply.github.com>
2023-11-12 13:37:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 from skorch import NeuralNetClassifier
 from sklearn.metrics import accuracy_score
 import torch
@@ -66,8 +67,31 @@ def plot_example(X, y):
 def home_page():
     st.title("Home Page")
     st.write("This is an app based on the classification of digits based on MNIST data.")
-    # Add content specific to the home page
+    st.write('The MNIST dataset is a collection of 28 x 28 pixel grayscale images of handwritten digits (0 through 9). It is a widely used dataset in machine learning and computer vision for training and testing various image processing systems. The name "MNIST" stands for Modified National Institute of Standards and Technology.')
+    image = Image.open('mnist.webp')
+    st.image(image , caption='This is the dataset which you will train ')
 
+    st.header('Now what is Machine Learning ?')
+    st.write('Machine learning is a subset of artificial intelligence (AI) that focuses on the development of algorithms and models that enable computer systems to learn from data and improve their performance on a specific task without being explicitly programmed. In other words, machine learning involves the development of systems that can automatically learn and make predictions or decisions based on data.')
+
+    image2=Image.open('public_images_1605842918803-AI+vs+ML+vs+DL.png')
+    st.image(image2,caption = 'DL is what we will focus more')
+    # Add content specific to the home page
+    st.header("MACHINE LEARNING OR DEEP LEARNING PIPELINE:")
+    image3=Image.open('manual-pipeline.png')
+    st.image(image3,caption = "This is a general pipeline")
+    st.text('but we will not focus this much in depth at the level of class 5-10')
+
+    st.header("But what is a MODEL ?")
+    st.write("A machine learning (ML) model is a mathematical representation or a computational system that learns patterns from data and makes predictions or decisions without being explicitly programmed for the task. In other words, a machine learning model is trained on historical data to recognize patterns and relationships, and it can then use that knowledge to make predictions or decisions on new, unseen data.")
+
+    st.write("Our model typically consists of several key components designed to effectively process and classify images. Here's a theoretical overview of a simple CNN architecture for MNIST classification:")
+    image4 = Image.open('cnn.webp')
+    st.image(image4)
+
+    st.write('Now we will proceed towards our ML model :muscle: 	:female-technologist:')
+    st.text('SO SIT BACK,RELAX AND ENJOY IT !	 ')
+    st.write('	:tropical_drink: :tropical_drink:')
 def train():
     test_size_key = "test_size_slider"
     random_state_key = "random_state_slider"
@@ -93,7 +117,7 @@ def train():
 
     assert(X_train.shape[0] + X_test.shape[0] == mnist.data.shape[0])
     st.write('X_train =',  X_train.shape)
-    st.write('y_train =', y_train.shape)
+    #st.write('y_train =', y_train.shape)
 
     if st.button('Show example images'):
         fig = plot_example(X_train, y_train)
@@ -107,12 +131,13 @@ def train():
 
     mnist_dim, hidden_dim, output_dim
 
+
     torch.manual_seed(0)
     epoch = st.number_input("ENTER EPOCH", min_value=1, step=1, value=20,key = epoch_key)
     lr = st.slider("ENTER Learning Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.1, format='%0.1f',key=lr_key)
     
     dropout = st.slider("ENTER DROPOUT",min_value=0.0,max_value=1.0,value=0.2,step=0.1,format='%0.2f',key=dropout_key)
-   
+
     
     class ClassifierModule(nn.Module):
         def __init__(
@@ -135,10 +160,10 @@ def train():
             return X
         
 
-   
-   # X_accumulated = np.empty_like(X_train[:0])
-   # y_accumulated = np.empty_like(y_train[:0])
-    if st.button('SHOW ITERATIONS'):
+
+# X_accumulated = np.empty_like(X_train[:0])
+# y_accumulated = np.empty_like(y_train[:0])
+    if st.toggle('SHOW ITERATIONS'):
         for i in range(epoch):
 
             net = NeuralNetClassifier(
@@ -156,7 +181,8 @@ def train():
 
             y_pred = net.predict(X_test)
             acc = accuracy_score(y_test, y_pred)
-            st.write(f"In the iteration {i+1} the accuracy is {acc : 0.3f} ")
+            st.write(f"EPOCH : {i+1}   ACCURACY_SCORE-->{acc : 0.3f} ")
+            return net
         
     else :
         net = NeuralNetClassifier(
@@ -171,15 +197,15 @@ def train():
         acc = accuracy_score(y_test, y_pred)
     
     st.write("ACCURACY SCORE - ", accuracy_score(y_test, y_pred))
-   
-    # Add content specific to the about page
+    
+        # Add content specific to the about page
     return net
 
 
 def test(net):
     st.title("TESTING THE DATA")
 
-    uploaded_file = st.file_uploader(label='Pick an image to test that is a number from 1 to 9')
+    uploaded_file = st.file_uploader(label='Pick an image depicting a single digit from 0 to 9')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         image = Image.open(uploaded_file).convert("L").resize((28, 28))
@@ -198,8 +224,13 @@ def test(net):
 
         # Ensure prediction is a 1D array before converting to int
         predicted_digit = int(prediction[0] if prediction.ndim > 1 else prediction)
-        st.write("Predicted Digit:", predicted_digit)
-    
+        st.write("Predicted Digit:   ", predicted_digit)
+
+        ANS = st.radio('Is the Prediction Correct ?',options=['True','False'],index=None)
+        if ANS=='True':
+            st.balloons()
+            
+            
     if st.button('Use canvas to predict') :
         drawing_mode = st.sidebar.selectbox(
         "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
@@ -234,6 +265,7 @@ def test(net):
 # Do something interesting with the image data and paths
         if canvas_result.image_data is not None:
             st.image(canvas_result.image_data)
+        
 
 
 def main():
@@ -245,6 +277,7 @@ def main():
     if selected_page == "Home":
         home_page()
     elif selected_page == "Train":
+        
         net=train()
         test(net)
     #elif selected_page == "Test":
